edb094c2194b5283efd44ed5f9402a83393c05d1
KAMRAN ANWAR <KMRNNWR11GMAIL.COM>
2025-12-25 09:49:24 +0100

    chore: update web build with video fix

--- patente_b_flutter/build/web/flutter_bootstrap.js
+++ patente_b_flutter/build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "933307723"
+    serviceWorkerVersion: "1057486114"
   }
 });
